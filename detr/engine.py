60bc0fb5d604b5042cb9c13c4cc4df1df35503b8
Neil <41444780+Neveon@users.noreply.github.com>
2021-08-12 10:27:04 -0400

    Update engine.py

--- detr/engine.py
+++ detr/engine.py
@@ -87,8 +87,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
 
     print(metric_logger.log_every(data_loader, 10, header))
     print(type(metric_logger.log_every(data_loader, 10, header)))
-    print(metric_logger.log_every(data_loader, 10, header).shape)
-#     for samples, targets in metric_logger.log_every(data_loader, 10, header):
+    for samples, targets in metric_logger.log_every(data_loader, 10, header):
 #         samples = samples.to(device)
 #         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
