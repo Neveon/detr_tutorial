a79ca7e4775d02095e648a0689211aae6db11a87
Neveon <41444780+Neveon@users.noreply.github.com>
2021-09-28 16:13:19 -0400

    debugging build_dataset

--- detr/datasets/__init__.py
+++ detr/datasets/__init__.py
@@ -17,6 +17,8 @@ def get_coco_api_from_dataset(dataset):
 
 
 def build_dataset(image_set, args):
+    print("Building datset..")
+    print("DATASET FILE: ", args.dataset_file)
     if args.dataset_file == 'coco':
         return build_coco(image_set, args)
     if args.dataset_file == 'face':
